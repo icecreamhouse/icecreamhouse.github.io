a3a1691fdb8ed98ddc86ea3103cb37e5c7959bb0
icecreamhouse <ainsleyj80@gmail.com>
2014-08-11 21:08:27 +1200

    14

--- app.js
+++ app.js
@@ -1,14 +1,8 @@
 
-var main = function(){
-	$('plane').click(function(){
-	/*document.getElementById("one").innerHTML = 'new text'; */
-	$('one').animate({
-	
-	right: "100px"
-	},200);
-	});
+
+window.onload = function() {
+
+alert("welcome");
 
 
-};
-$(document).ready(main);
-/*<script src="jquery.min.js"></script>*/
+}
