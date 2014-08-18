eb2241c231ca167ba34f118326c04b75b809f05a
icecreamhouse <ainsleyj80@gmail.com>
2014-08-18 21:13:52 +1200

    71

--- app.js
+++ app.js
@@ -17,7 +17,9 @@ $("#browse").click(function(){
 	$('#contact-wrap').animate(
 		{ right: '-100'},
 		500);
-
+	$('#contact-wrap').animate(
+		{ left: '100'},
+		500);
 });
 
 $(".icon-menu").click(function(){
